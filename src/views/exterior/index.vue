acebe593216d2e849b2c6fa292dc1a061ca3507f
TiAmo <361258177@qq.com>
2024-05-22 18:32:13 +0800

    feat: exterior(未完成)

---
+++ src/views/exterior/index.vue
@@ -0,0 +1,38 @@
+<template>
+    <div id="app">
+        <Header></Header>
+        <Nav @statusChange="statusChange"></Nav>
+        <Main :class="navStatusClass" :withoutRight="true">
+            <div class="m-main">
+                <keep-alive>
+                    <router-view></router-view>
+                </keep-alive>
+            </div>
+        </Main>
+    </div>
+</template>
+
+<script>
+import Nav from "@/components/Nav_v4.vue";
+export default {
+    name: "App",
+    provide: {},
+    data: function () {
+        return {
+            navStatusClass: "is-regular",
+        };
+    },
+    mounted() {},
+    computed: {},
+    methods: {
+        statusChange(navStatusClass) {
+            this.navStatusClass = navStatusClass;
+        },
+    },
+    components: { Nav },
+};
+</script>
+
+<style lang="less">
+@import "~@/assets/css/app.less";
+</style>
